fb75e5a49f062a2e4cd4c0911bf03c001639f59b
dani <photomassacre@gmail.com>
2022-04-15 18:02:57 -0700

    Updated service-worker.js to be lean

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,7 +1,8 @@
 const CACHE_NAME = "budget-cache";
 const DATA_CACHE_NAME = "budget-data-cache";
-const APP_PREIX = 'Budget Tracker-O-rama';
+// const APP_PREFIX = 'Budget Tracker-O-rama';
 const VERSION = 'version_01';
+// const CACHE_NAME = APP_PREFIX + VERSION;
 
 
 
@@ -18,6 +19,7 @@ self.addEventListener('install', function (e) {
     caches.open(CACHE_NAME).then(cache => {
       console.log('Your files were pre-cached successfully!' + CACHE_NAME);
       return cache.addAll(FILES_TO_CACHE);
+      //this line adds all of the files associated with the cache name
     })
   );
 
@@ -43,44 +45,56 @@ self.addEventListener('activate', function (e) {
 });
 
 // Intercept fetch requests
-self.addEventListener('fetch', function (e) {
-  if (e.request.url.includes('/api/')) {
-    e.respondWith(
-      caches
-        .open(DATA_CACHE_NAME)
-        .then(cache => {
-          return fetch(e.request)
-            .then(response => {
-              // If the response was good, clone it and store it in the cache.
-              if (response.status === 200) {
-                cache.put(e.request.url, response.clone());
-              }
+// self.addEventListener('fetch', function (e) {
+//   if (e.request.url.includes('/api/')) {
+//     e.respondWith(
+//       caches
+//         .open(DATA_CACHE_NAME)
+//         .then(cache => {
+//           return fetch(e.request)
+//             .then(response => {
+//               // If the response was good, clone it and store it in the cache.
+//               if (response.status === 200) {
+//                 cache.put(e.request.url, response.clone());
+//               }
 
-              return response;
-            })
-            .catch(err => {
-              // Network request failed, try to get it from the cache.
-              return cache.match(e.request);
-            });
-        })
-        .catch(err => console.log(err))
-    );
+//               return response;
+//             })
+//             .catch(err => {
+//               // Network request failed, try to get it from the cache.
+//               return cache.match(e.request);
+//             });
+//         })
+//         .catch(err => console.log(err))
+//     );
 
-    return;
-  }
+//     return;
+//   }
 
+//   e.respondWith(
+//     fetch(e.request).catch(function () {
+//       return caches.match(e.request).then(function (response) {
+//         if (response) {
+//           return response;
+//         } else if (e.request.headers.get('accept').includes('text/html')) {
+//           // return the cached home page for all requests for html pages
+//           return caches.match('/');
+//         }
+//       });
+//     })
+//   );
+// });
+
+self.addEventListener("fetch", function (e) {
   e.respondWith(
-    fetch(e.request).catch(function () {
-      return caches.match(e.request).then(function (response) {
-        if (response) {
-          return response;
-        } else if (e.request.headers.get('accept').includes('text/html')) {
-          // return the cached home page for all requests for html pages
-          return caches.match('/');
-        }
-      });
+    caches.match(e.request).then(function (request) {
+      if (request) {
+        return request;
+      } else {
+        return fetch(e.request)
+      }
     })
-  );
-});
+  )
+})
 
 
